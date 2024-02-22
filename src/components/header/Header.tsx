bfc896635bb6b6635b7a7b1391fc2977057a1436
devalecf <afessler1998@gmail.com>
2024-02-22 15:05:25 -0600

    header fully responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,15 +10,23 @@ const HeaderContainer = styled.header`
   max-width: 100vw;
   text-align: right;
   padding: 1rem 2rem 1rem 0;
-  font-size: 1.2rem;
+  font-size: clamp(1.25rem, 1vw + 1rem, 2.5rem);
   position: fixed;
   top: 0;
   background-color: ${({ theme }) => theme.colors.background};
   z-index: 1000;
+  @media (min-width: 1440px) {
+    padding: 2rem 2rem 2rem 0;
+    font-size: clamp(2.5rem, 2vw, 3.5rem);
+  }
+  @media (min-width: 1920px) {
+    padding: 3rem 2rem 3rem 0;
+    font-size: clamp(3.5rem, 2vw, 4.5rem);
+  }
 `;
 
 const ScrollNav = styled(ScrollLink)`
-  margin-left: 1rem;
+  margin-left: clamp(1rem, 2vw, 3rem);
   cursor: pointer;
   transition: color 0.2s;
   color: ${({ theme }) => theme.colors.text};
@@ -30,7 +38,7 @@ const ScrollNav = styled(ScrollLink)`
 `;
 
 const RouterNav = styled(Link)`
-  margin-left: 1rem;
+  margin-left: clamp(1rem, 2vw, 3rem);
   cursor: pointer;
   transition: color 0.2s;
   color: ${({ theme }) => theme.colors.text};
